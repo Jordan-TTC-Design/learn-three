3351e397a0e58b10bd094c8a1194c7d6bb02f23e
JordanTseng <jordan.ttc.design@gmail.com>
2023-04-02 14:43:14 +0800

    feat: import 3d models

---
+++ pages/advance/import-model.vue
@@ -0,0 +1,183 @@
+<script setup>
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import * as dat from 'lil-gui';
+
+const threeScene = ref(null);
+onMounted(() => {
+  /**
+ * Debug
+ */
+  const gui = new dat.GUI()
+
+  // model
+  const dracoLoader = new DRACOLoader()
+  dracoLoader.setDecoderPath('/draco/')
+
+  const gltfLoader = new GLTFLoader()
+  gltfLoader.setDRACOLoader(dracoLoader)
+  let mixer = null
+  gltfLoader.load(
+    '/models/Fox/glTF/Fox.gltf',
+    (gltf) => {
+      console.log(gltf)
+      gltf.scene.scale.set(0.025, 0.025, 0.025)
+      scene.add(gltf.scene)
+      mixer = new THREE.AnimationMixer(gltf.scene)
+      const action = mixer.clipAction(gltf.animations[0])
+      action.play()
+    },
+  )
+
+  /**
+ * Base
+ */
+  // Canvas
+  // const canvas = document.querySelector('canvas.webgl')
+
+  // Scene
+  const scene = new THREE.Scene()
+
+  /**
+ * Textures
+ */
+  // const textureLoader = new THREE.TextureLoader()
+  const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+  const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.png',
+    '/textures/environmentMaps/0/nx.png',
+    '/textures/environmentMaps/0/py.png',
+    '/textures/environmentMaps/0/ny.png',
+    '/textures/environmentMaps/0/pz.png',
+    '/textures/environmentMaps/0/nz.png'
+  ])
+
+  /**
+ * Floor
+ */
+  const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+      color: '#777777',
+      metalness: 0.3,
+      roughness: 0.4,
+      envMap: environmentMapTexture,
+      envMapIntensity: 0.5
+    })
+  )
+  floor.receiveShadow = true
+  floor.rotation.x = -Math.PI * 0.5
+  scene.add(floor)
+
+  /**
+ * Lights
+ */
+  const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.7)
+  scene.add(ambientLight)
+
+  const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.2)
+  directionalLight.castShadow = true
+  directionalLight.shadow.mapSize.set(1024, 1024)
+  directionalLight.shadow.camera.far = 15
+  directionalLight.shadow.camera.left = -7
+  directionalLight.shadow.camera.top = 7
+  directionalLight.shadow.camera.right = 7
+  directionalLight.shadow.camera.bottom = -7
+  directionalLight.position.set(5, 5, 5)
+  scene.add(directionalLight)
+
+  /**
+ * Sizes
+ */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+
+  window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // 反推 sphereBody 的位置
+    sphereBody.applyForse(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position)
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+
+  /**
+ * Camera
+ */
+  // Base camera
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+  camera.position.set(-3, 3, 3)
+  scene.add(camera)
+
+  // Controls
+  const controls = new OrbitControls(camera, threeScene.value)
+  controls.enableDamping = true
+
+  /**
+ * Renderer
+ */
+  const renderer = new THREE.WebGLRenderer({
+    canvas: threeScene.value,
+  })
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  /**
+ * Animate
+ */
+  const clock = new THREE.Clock()
+  let oldTime = 0;
+  const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldTime;
+    oldTime = elapsedTime;
+    // Update controls
+    controls.update()
+
+    // Update mixer
+    if (mixer) {
+      mixer.update(deltaTime)
+    }
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+  }
+
+  tick()
+});
+</script>
+
+<template>
+  <div>
+    <canvas id="threeScene" ref="threeScene"></canvas>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#threeScene {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  outline: none;
+}
+</style>
